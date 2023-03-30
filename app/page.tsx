264e1c9b4f9ec1525a8bd5248f9f362ef38cadba
Kevin Lam <mtn013209@gmail.com>
2023-03-30 10:08:16 -0700

    a

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { BowlControls, BowlRender } from "../components/index"
 export default function Home() {
   return (
     <main>
+      we're in the home page
       <BowlRender />
       <BowlControls />
     </main>
