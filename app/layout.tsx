c726e3ad3c27aae038cfbb31ba634627e29c0401
Kevin Lam <mtn013209@gmail.com>
2023-04-20 12:44:04 -0700

    cron

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import "@picocss/pico/css/pico.min.css"
 import "bulma/css/bulma.min.css"
+import { useCronitor } from "@cronitorio/cronitor-rum-nextjs"
 
 // this is how you use... any? css in react.
 import { Navbar, Footer } from "../components/index"
@@ -15,6 +16,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  useCronitor("8ab20f019d3ec66a9196a1919d34cfa7")
+
   return (
     <html lang="en">
       <body>
